0d0fc693ceb984979093728bd0c217d355da86d5
Soichi Watanabe <soi.watanabe.chi@gmail.com>
2023-07-01 18:14:05 +0900

    add unary

--- 9cc.c
+++ 9cc.c
@@ -161,8 +161,10 @@ Token *tokenize() {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
+// expr    = mul ("+" mul | "-" mul)*
 Node *expr() {
 	Node *node = mul();
 
@@ -176,18 +178,29 @@ Node *expr() {
 	}
 }
 
+//mul     = unary ("*" unary | "/" unary)*
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
 }
 
+// unary   = ("+" | "-")? unary | primary
+Node *unary() {
+	if (consume('+'))
+		return unary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), unary());
+	return primary();
+}
+
+//primary = "(" expr ")" | num
 Node *primary() {
 	// if next token is "(" then, "(" expr ")".
 	if (consume('(')) {
--- test.sh
+++ test.sh
@@ -26,5 +26,13 @@ assert 17 "1-(2+  3)+7  +( 44  - 30)"
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
+assert 10 '-10+20'
+assert 30 '+10++20'
+assert 2 '- 3 +5'
+assert 17 '- 2 -    3 +22'
+assert 10 '-4 ++11 -   2 ++5'
+assert 10 '- (-10)'
+assert 10 '- -10'
+assert 10 '- - +10'
 
 echo OK
