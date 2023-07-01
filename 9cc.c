071e5928ff02472495d5aacf78bca164e9bfe4ba
Soichi Watanabe <soi.watanabe.chi@gmail.com>
2023-07-01 17:17:17 +0900

    add Recursive Descent Parsing

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,38 @@
 #include <stdarg.h>
 #include <string.h>
 
+// kind of abstract syntax tree
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+// AST node type
+struct Node {
+	NodeKind kind;  // node type
+	Node *lhs;      // left hand side
+	Node *rhs; 		// right hand side
+	int val;   		// use when kind is ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs  = lhs;
+	node->rhs  = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
 // kind of token
 typedef enum {
 	TK_RESERVED, // kigou
@@ -125,6 +157,51 @@ Token *tokenize() {
 	return head.next;
 }
 
+Node *primary() {
+	return new_node_num(expect_number());
+}
+
+Node *mul() {
+	Node *node = primary();
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch (node->kind) {
+		case ND_ADD:
+			printf("	add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("	sub rax, rdi\n");
+			break;
+	}
+	printf("	push rax\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "don't correct num of arg\n");
@@ -134,25 +211,19 @@ int main(int argc, char **argv) {
 	// tokenize
 	user_input = argv[1];
 	token = tokenize();
+	Node *node = expr();
 
 	// first half of ascenbry
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// String in first of formula must be number.
-	printf("	mov rax, %ld\n", expect_number());
+	// we generate code while wolking down AST
+	gen(node);
 
-	// cunsume token that `+ <num>` or `- <num>`, and output ascenbry
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("	add rax, %ld\n", expect_number());
-			continue;
-		}
-		expect('-');
-		printf("	sub rax, %ld\n", expect_number());
-	}
-
+	// Top of Stack have return value of input,
+	// therefor we load the value to RAX and return.
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
